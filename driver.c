f691b26490bbf9fab07e2eed03297407f4e7408f
is0p0d <jpmoroney715@outlook.com>
2023-12-08 07:46:16 -0600

    hehe

--- driver.c
+++ driver.c
@@ -1,1 +1,23 @@
 //jpm 2023
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+#include <time.h>
+//#include <mpi.h>
+
+int main(int argc, char* argv[])
+{
+    if (argc < 1)
+    {
+        printf("ERROR: You must specify an array size.\n");
+        return 1;
+    }
+    
+    uint64_t matrixSize = atoi(argv[1]);
+
+    //adjust the matrix size to account for our heating elements
+    uint64_t matRow = matrixSize+2;
+    uint64_t matCol = matrixSize+2;
+
+}
